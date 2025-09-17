6a6c9f5fd9aa663117932c828403bf6fe4a33bca
Tatiana V <tatianavanegas2006@gmail.com>
2025-09-17 16:04:50 -0500

    Modifique para pasar múltiples props

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Usuario from '../components/Usuario';
+
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Actividad de Props en Next.js</h1>
+      <Usuario nombre="Ana García" edad={25} ciudad="Madrid" />
     </main>
   );
 }
